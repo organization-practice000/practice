0c56955133111754ad76f3bca79e6a063aeed4b6
DESKTOP-E9CUUFE\804-19 <cacao3216@gmail.com>
2024-02-16 10:59:07 +0900

    first commit

---
+++ src/main/java/org/example/ohgiraffers/prprpr/PrprprApplication.java
@@ -0,0 +1,13 @@
+package org.example.ohgiraffers.prprpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrprprApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrprprApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ohgiraffers/prprpr/PrprprApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ohgiraffers.prprpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrprprApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
